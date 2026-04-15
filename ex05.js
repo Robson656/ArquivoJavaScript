37fe0d9ad935cd593af8208452ddd6e6c767eb71
Robson656 <suairma1598@gmail.com>
2026-04-15 17:06:53 -0300

    Add files via upload

---
+++ ex05.js
@@ -0,0 +1,14 @@
+let num = 1;
+while (num <=10){
+    console.log(num);
+    num++;
+}
+
+// numero par
+let num2 = 1;
+while (num2 <=10){
+    num2++;
+    if (num2 % 2 === 0){
+        console.log(num2)
+    }
+}
